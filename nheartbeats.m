546999510e2fd1a780a10c94d2159936c8db6701
froth30 <theo.frohlich@gmail.com>
2017-04-13 15:04:28 -0400

    Modified to take 'MinPeakHeight' property optionally

--- nheartbeats.m
+++ nheartbeats.m
@@ -1,21 +1,25 @@
-function nbeats = nheartbeats(data, properties)
+function nbeats = nheartbeats(data, varargin)
   %NHEARTBEATS Determines the number of heart beats in the given EKG sample
   %data interval.
   %   Param:
-  %      data       - an EKG data interval, represented as a 1-D vector
-  %      properties - a struct containing a value for 'MinPeakHeight'
+  %      data - an EKG data interval, represented as a 1-D vector
+  %      mph  - 'MinPeakHeight' property (optional, 800 by default)
   %   Return:
   %      nbeats - the number of heart beats
   %
   %   Since:  April 13, 2017
-  %   Author: Ted Frohlich (ttf10@case.edu)
+  %   Author: Ted Frohlich <ttf10@case.edu>
+  
+  if (nargin > 0)
+    mph = varargin{1};
+  else
+    mph = 800;
+  end
   
   next = zeros(size(data));
   next(1:end-1) = data(2:end);
   
-  mph = properties.MinPeakHeight;
   ibeats = find(data <= mph & next > mph);
-  
   nbeats = length(ibeats);
   
 end
